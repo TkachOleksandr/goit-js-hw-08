90ce580272035dc8db35c3135f1c470476956937
Oleksand Tkach <sashatkach66@gmail.com>
2025-01-07 20:33:25 +0200

    message commit

--- js/gallery.js
+++ js/gallery.js
@@ -63,4 +63,50 @@ const images = [
       description: 'Lighthouse Coast Sea',
     },
   ];
+  const gallery = document.querySelector('.gallery');
+
+  gallery.addEventListener('click', onClickOpenImg);
   
+  const imgItems = images.map(({ preview, original, description }) =>
+      ` <li class="gallery-item">
+          <a class="gallery-link" href="${original}">
+              <img
+                  class="gallery-image"
+                  src="${preview}"
+                  data-source="${original}"
+                  alt="${description}"
+                  width='360'
+              />
+          </a>
+      </li>`
+  ).join('');
+  
+  gallery.insertAdjacentHTML('beforeend', imgItems);
+  
+  function onClickOpenImg(event) {
+      event.preventDefault();
+  
+      const { target } = event;
+
+  
+      if (target.nodeName !== "IMG") {
+          return;
+      }
+  
+      const instance = basicLightbox.create(
+          `<img src="${target.dataset.source}" alt="${target.alt}" width="1280">`
+      )
+      instance.show();
+  
+      const keyPress = event => {
+          if (event.code === 'Escape') {
+              instance.close();
+              document.removeEventListener('keydown', keyPress);
+          }
+      };
+  
+      if (instance.show()) {
+          document.addEventListener('keydown', keyPress);
+      }
+  
+  }
